4232bf5ff9c5ab4c4b76ee62efcb9c2becf3b5c8
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-18 10:54:25 +0200

    feat: UI added, with counters and face
    App has now the classical minesweeper interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* Main */
 
-function App() {
+import React from "react";
+
+/* Components */
+
+import NumberDisplay from "./components/NumberDisplay/index";
+
+import "./App.scss";
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">😊</div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">Body</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NumberDisplay/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./NumberDisplay.scss";
+
+interface NumberDisplayProps {
+  value: number;
+}
+
+const NumberDisplay: React.FC<NumberDisplayProps> = ({ value }) => {
+  return (
+    <div className="number-display">{value.toString().padStart(3, "0")}</div>
+  );
+};
+
+export default NumberDisplay;
