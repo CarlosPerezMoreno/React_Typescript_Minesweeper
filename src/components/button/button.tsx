79601ea8b61259c19e55a269f5022f782f14690c
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-20 17:53:08 +0200

    feat: Mines logic set up. Game win and lost configured
    Game is almost completed

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,18 +7,20 @@ interface ButtonProps {
   col: number;
   onClick(rowParam: number, colParam: number): (...args: any[]) => void;
   onContext(rowParam: number, colParam: number): (...args: any[]) => void;
+  red?: boolean;
   row: number;
   state: CellState;
   value: CellValue;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  state,
-  value,
-  row,
   col,
   onClick,
   onContext,
+  red,
+  row,
+  state,
+  value,
 }) => {
   const renderContent = (): React.ReactNode => {
     if (state === CellState.visible) {
@@ -47,7 +49,7 @@ const Button: React.FC<ButtonProps> = ({
     <div
       className={`slot-button ${
         state === CellState.visible ? "visible-button" : ""
-      } valueIs${value}`}
+      } valueIs${value} ${red ? "red" : ""}`}
       onClick={onClick(row, col)}
       onContextMenu={onContext(row, col)}
     >
